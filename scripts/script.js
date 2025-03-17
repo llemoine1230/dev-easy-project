bf0ebba6b5ee0dcd28334eafab954346d6bef2c0
dgoodwin-maker <dgoodwin3@una.edu>
2025-03-17 03:31:29 -0500

    Update script.js
    Our array

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,66 @@
-gdg
+// let card1 = "My random fact is that I have two brothers";
+// let card2 = "My fav color is purple.";
+// let card3 = "I like to swim";
+// let card4 = "I broke my finger";
+// let card5 = "My name is Victor, and I'm ambidextrous.";
+// let card6 = "I can say my alphabet backwards";
+// let card7 = "I am not famous";
+// let card8 = "I was born in Louisiana";
+// let card9 = "I have never had a Peanut Butter and Jelly Sandwich!";
+// let card10 = "I am allergic to cats";
+// let card11 = "I like trains.";
+// let card12 = "I know the secret identity of beastcarp1";
+// let card13 = "I have a dog";
+// let card14 = "I hate UNA parking";
+// let card15 = "I have 3 dogs";
+// let card16 = "I'm adopted from China :D";
+// let card17 = "I used to have an orange Hummer H3";
+// let card18 = "I play on the rugby team";
+// let card19 = "UNA basketball sold the bag";
+// let card20 = "The Great Wall of China is 13,000 miles long";
+// let card21 = "I hate javascript.";
+// let card22 = "I love the Bible verse Romans 5:8!";
+// let card23 = "I love running.";
+// let card24 = "I have eaten 1 pound of ground beef every day for the last 6 months.";
+// let card25 = "Still very less parking space in UNA";
+// let card26 = "I enjoy kickboxing";
+// let card27 = "Filler";
+// let card28 = "Filler";
+
+// We need 28 total correct? Only have 26.
+// Might can make this into a loop? 
+
+
+// Another Array format:
+
+// let facts = [
+//   "My random fact is that I have two brothers",
+//   "My fav color is purple.",
+//   "I like to swim",
+//   "I broke my finger",
+//   "My name is Victor, and I'm ambidextrous.",
+//   "I can say my alphabet backwards",
+//   "I am not famous",
+//   "I was born in Louisiana",
+//   "I have never had a Peanut Butter and Jelly Sandwich!",
+//   "I am allergic to cats",
+//   "I like trains.",
+//   "I know the secret identity of beastcarp1",
+//   "I have a dog",
+//   "I hate UNA parking",
+//   "I have 3 dogs",
+//   "I'm adopted from China :D",
+//   "I used to have an orange Hummer H3",
+//   "I play on the rugby team",
+//   "UNA basketball sold the bag",
+//   "The Great Wall of China is 13,000 miles long",
+//   "I hate javascript.",
+//   "I love the Bible verse Romans 5:8!",
+//   "I love running.",
+//   "I have eaten 1 pound of ground beef every day for the last 6 months.",
+//   "Still very less parking space in UNA",
+//   "I enjoy kickboxing",
+//   "Filler",
+//   "Filler"
+// ];
+ 
